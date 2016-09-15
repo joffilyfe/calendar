10bfba770db9b3fe87a180c3b3f88c84ff5c0b7f
Joffily Ferreira <joffily@outlook.com>
2016-09-15 13:57:32 -0300

    Created a function to check admin user status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def user_is_admin?
+    unless user_signed_in? and current_user.admin
+      redirect_to root_path
+    end
+  end
 end
