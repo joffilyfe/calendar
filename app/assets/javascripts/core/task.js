d4b25422f36864f5b9285747b5db2ffb76cc21d8
Joffily Ferreira <joffily@outlook.com>
2016-09-15 21:52:30 -0300

    Removed turbolins

---
+++ app/assets/javascripts/core/task.js
@@ -0,0 +1,51 @@
+var app = {};
+app.atartApp = function() {
+  $.getJSON('/api/v1/task', function(response) {
+    var events = response.map(function(event) {
+      return {title: event.title, description: event.description, start: event.date, url: '/core/tasks/' + event.id + '/edit' };
+    })
+
+    $('#calendar').fullCalendar({
+      customButtons: {
+        newTaskButton: {
+            text: 'New Task',
+            click: function() {
+                window.location.href = '/core/tasks/new'
+            }
+        }
+      },
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'newTaskButton'
+      },
+      defaultDate: '2016-09-12',
+      navLinks: true,
+      editable: false,
+      eventLimit: true,
+      events: events,
+      selectable: false,
+      selectHelper: true,
+      select: function(start, end) {
+        var title = prompt('Event Title:');
+        var eventData;
+        if (title && title.length >= 5) {
+          eventData = { title: title, start: start };
+          $.ajax({
+            method: 'POST',
+            url: '/api/v1/task',
+            data: { title: title, start: start.toString()},
+          }).done(function(e) {
+            console.log(e);
+            $('#calendar').fullCalendar('renderEvent', eventData, true);
+          }).error(function(e) {
+            console.log(e);
+          });
+        } else {
+          alert("You need pass a title with length bit than 5 character.");
+        }
+        $('#calendar').fullCalendar('unselect');
+      },
+    });
+  });
+}
